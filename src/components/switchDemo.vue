c4258a654f4e4231db2f4b5f66bd35b342a92725
467112199@qq.com <467112199@qq.com>
2020-08-01 17:58:20 +0800

    switch外部传值切修改状态

--- src/components/switchDemo.vue
+++ src/components/switchDemo.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
-    <Switch />
+    <Switch :value='checked' @input="checked = $event" />
   </div>
 </template>
 <script lang="ts">
 import Switch from "../lib/switch.vue";
+import { ref } from 'vue';
 export default {
   components: { Switch },
+  setup(){
+      const checked = ref(false)
+      return {checked}
+  }
 };
 </script>
--- src/lib/switch.vue
+++ src/lib/switch.vue
@@ -1,17 +1,17 @@
 <template>
-  <button @click="toggle" :class="{checked}">
+  <button @click="toggle" :class="{checked:value}">
     <span></span>
   </button>
 </template>
 <script lang='ts'>
 import { ref } from "vue";
 export default {
-  setup() {
-    const checked = ref(false);
+  props: { value: Boolean },
+  setup(props, context) {
     const toggle = () => {
-      checked.value = !checked.value;
+      context.emit("input", !props.value);
     };
-    return { checked, toggle };
+    return { toggle };
   },
 };
 </script>
@@ -25,6 +25,7 @@ button {
   background: grey;
   border-radius: $h/2;
   position: relative;
+  cursor: pointer;
 }
 span {
   position: absolute;
@@ -34,11 +35,15 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2/2;
+  transition: left 250ms;
 }
 button.checked {
   background: blue;
 }
 button.checked > span {
   left: calc(100% - #{$h} - 2px);
+}
+button:focus {
+  outline: none;
 }
 </style>
