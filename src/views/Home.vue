b41d58dbb87aee74a04e1bacff0456b2de675bf8
467112199@qq.com <467112199@qq.com>
2020-08-03 22:47:28 +0800

    首页样式微调

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-<div>
+  <div>
     <top-nav />
     <div class="banner">
-      <h1>轱辘UI</h1>
-      <h2>一个厉害的 UI 框架</h2>
-      <p class="actions">
-        <a href="https://github.com">GitHub</a>
-        <router-link to="/doc">开始</router-link>
-      </p>
+      <div class="banner-top">
+        <div class="logo">
+          <img src="../assets/logo.png" alt />
+        </div>
+        <div class="desc">
+          <div class="desc-top">
+            <h1>TG-UI</h1>
+            <h2 style="margin-top:20px">好用vueUI框架</h2>
+          </div>
+          <p class="actions">
+            <a href="https://github.com">GitHub</a>
+            <router-link to="/doc">开始</router-link>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,22 +29,35 @@ export default {
 <style lang="scss" scoped>
 .banner {
   padding: 100px 0;
+  width:60%;
+  margin: 0 auto;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
-  > .actions{
-    padding: 8px 0;
-    a {
-      margin:0 8px;
-      background: #fff;
-      display: inline-block;
-      $h: 28px;
-      height: $h;
-      line-height: $h;
-      border-radius: $h/2;
-      padding: 0 8px;
+  > .banner-top {
+    display: flex;
+    > .logo {
+      padding-right: 80px;
+    }
+    > .desc {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      > .actions {
+        padding: 8px 0;
+        a {
+          margin: 0 8px;
+          background: #fff;
+          display: inline-block;
+          $h: 28px;
+          height: $h;
+          line-height: $h;
+          border-radius: $h/2;
+          padding: 0 8px;
+          border: 1px solid #ddd;
+        }
+      }
     }
   }
 }
