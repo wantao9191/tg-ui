0d54962038feff4b4c63fdda5df5f73c20dd6fcd
467112199@qq.com <467112199@qq.com>
2020-09-12 14:34:47 +0800

    diaolog基础样式 dialog 开启关闭功能

--- src/components/dialogDemo.vue
+++ src/components/dialogDemo.vue
@@ -1,11 +1,23 @@
 <template>
   <div>
-    <wt-dialog></wt-dialog>
+    <wt-button @click="toggle1">点击打开Dialog</wt-button>
+    <wt-dialog v-model:visible="dialogVisible1" :closeOnOverlay='false'>
+      <div slot="header">标题</div>
+    </wt-dialog>
   </div>
 </template>
 <script lang="ts">
 import dialog from "../lib/dialog.vue";
+import button from "../lib/button.vue";
+import { ref } from "vue";
 export default {
-  components: { wtDialog: dialog },
+  components: { wtDialog: dialog, wtButton: button },
+  setup() {
+    const dialogVisible1 = ref(false);
+    const toggle1 = () => {
+      dialogVisible1.value = true;
+    };
+    return { dialogVisible1, toggle1 };
+  },
 };
 </script>
--- src/lib/dialog.vue
+++ src/lib/dialog.vue
@@ -1,13 +1,68 @@
 <template>
-  <div class="wt-dialog-overlay">dialog</div>
-  <div class="wt-dialog-wrapper">
-    <header>标题</header>
-    <main>内容</main>
-    <footer>按钮</footer>
-  </div>
+  <template v-if="visible">
+    <div class="wt-dialog-overlay" @click="onClickOverlay"></div>
+    <div class="wt-dialog-wrapper">
+      <div class="wt-dialog">
+        <header slot="header"></header>
+        <main>内容</main>
+        <footer>按钮</footer>
+      </div>
+    </div>
+  </template>
 </template>
-<script>
-export default {};
+  
+</template>
+<script lang='ts'>
+export default {
+  props: {
+    visible: { type: Boolean, default: false },
+    closeOnOverlay: { type: Boolean, default: true },
+  },
+  setup(props, context) {
+    const close = () => {
+      context.emit("update:visible", false);
+      context.emit("close");
+    };
+    const onClickOverlay = () => {
+      if (props.closeOnOverlay) {
+        close();
+      }
+    };
+    return { close, onClickOverlay };
+  },
+};
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+.wt-dialog-overlay {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  background: rgba(0, 0, 0, 0.45);
+  z-index: 2000;
+}
+.wt-dialog-wrapper {
+  width: 50%;
+  background: #fff;
+  position: absolute;
+  top: 20vh;
+  left: 50%;
+  border-radius: 3px;
+  transform: translate(-50%, -50%);
+  z-index: 2001;
+  > .wt-dialog {
+    > header {
+      font-size: 18px;
+      padding: 20px 20px 10px;
+      font-weight: 400;
+    }
+    > main {
+      padding: 30px 20px;
+    }
+    > footer {
+      padding: 10px 20px 20px;
+    }
+  }
+}
 </style>
